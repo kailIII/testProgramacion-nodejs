656a468c1cb892390cf47891b6af28ebdf8487e0
Manuel Ortega Carcamo <mortegac@gmail.com>
2017-01-31 00:50:21 -0300

    Actualizacion archivo colegio y mazoCartas

--- diseno/lib/objMazo.js
+++ diseno/lib/objMazo.js
@@ -3,30 +3,20 @@
 =====================================================================*/
 	let Mazo = module.exports.Mazo = function(){
 		
-		
-	
-
-	//	this.carta=["as","dos","tres","cuatro","cinco","seis","siete","ocho","nueve","diez","jota","reina","rey"];
 		 
 	let aleaPinta = Math.floor((Math.random() * 4) + 1);
 	let aleaCarta = Math.floor((Math.random() * 13) + 1);
 
 	var pinta= ["Picas", "Corazones", "Diamantes", "Tréboles"];
+	var carta=["as","dos","tres","cuatro","cinco","seis","siete","ocho","nueve","diez","jota","reina","rey"];
 
  	this.devPinta = function() {
- 			
-			return this.pinta[2];
-			//return 4;
+			return this.pinta[this.aleaPinta];
 	  	};
 
- 		// this.devCarta = function() {
-			// return this.carta(8);
-		 //  	};
-
-
-		// this.pinta=pinta(Math.floor((Math.random() * 4) + 1));
-		// this.carta=carta(Math.floor((Math.random() * 13) + 1));
-		
+ 	this.devCarta = function() {
+			return this.carta[this.aleaCarta];
+	  	};
 		  
 		
 	return this;
--- diseno/mazoCartas.js
+++ diseno/mazoCartas.js
@@ -4,4 +4,4 @@ const mazo = require('./lib/objMazo');
 let Mazo1 = new mazo.Mazo();
 
 console.log(Mazo1.devPinta());
-//console.log(Mazo1.devCarta());
+console.log(Mazo1.devCarta());
